415779e92905137ff6e6bdbf30974337c522c604
3103ski <bjastski@gmail.com>
2020-11-02 22:30:40 -0800

    rought layout for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,45 @@
 import React from 'react';
-import { Feature } from '../components';
+import * as colors from '../constants/colors';
+import { Feature, Footer, LayoutSection } from '../components';
 import { HeaderContainer } from '../containers';
-// import { Button } from 'reactstrap';
+import { Button, Col } from 'reactstrap';
 
 export default function About() {
 	return (
 		<>
-			<HeaderContainer>
-				<Feature>
-					<Feature.Title>Learn about ToDoIt</Feature.Title>
-				</Feature>
-			</HeaderContainer>
+			<HeaderContainer />
+			<LayoutSection>
+				<Col md={6}>
+					<Feature>
+						<Feature.Title>ToDOIT Has History!</Feature.Title>
+						<Feature.SubTitle>
+							Proident aliqua dolor dolore reprehenderit qui aute consectetur ut sunt. Consequat magna aliqua dolor cupidatat pariatur elit qui nulla pariatur dolor non qui. Enim dolor enim eiusmod
+							fugiat qui ullamco. Tempor in deserunt culpa deserunt culpa do enim proident ad do. Eu velit veniam eiusmod deserunt sunt. Sit ullamco fugiat do deserunt nulla ullamco ea officia
+							proident nisi do dolore.
+						</Feature.SubTitle>
+					</Feature>
+				</Col>
+				<Col md={6}>
+					<Feature>
+						<img src='/assets/images/checklist.png' alt='checklist' />
+					</Feature>
+				</Col>
+				<Col>
+					<Feature>
+						<Feature.SubTitle>So many are out there beign productive!</Feature.SubTitle>
+						<Button>Sign Up</Button>
+					</Feature>
+				</Col>
+			</LayoutSection>
+			<LayoutSection fullWidth containerColor={colors.ligthPrimary}>
+				<Col>
+					<Feature>
+						<Feature.Title>CONTACT US</Feature.Title>
+						<Feature.SubTitle>EVENTUALLY THIS SPACE WILL BE FILLED UP WITH A FORM</Feature.SubTitle>
+					</Feature>
+				</Col>
+			</LayoutSection>
+			<Footer />
 		</>
 	);
 }
